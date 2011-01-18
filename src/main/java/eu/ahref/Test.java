bbd08fe954366b12c2360068bd163172943a4a8b
martino <martino@ahref.eu>
2011-01-18 17:26:06 +0100

    Fixed pom.xml target jvm

---
+++ src/main/java/eu/ahref/Test.java
@@ -0,0 +1,11 @@
+package eu.ahref;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: martino
+ * Date: 14/01/11
+ * Time: 11.49
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
