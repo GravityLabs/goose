3d2ca3d69f48a1defee9883a6fe2afde36b32a0f
Mark Needham <m.h.needham@gmail.com>
2012-02-04 14:38:03 +0000

    mark: punctuation stuff only used by one method so push them together

--- src/main/scala/com/gravity/goose/text/StopWords.scala
+++ src/main/scala/com/gravity/goose/text/StopWords.scala
@@ -26,12 +26,9 @@ package com.gravity.goose.text
 
 import java.util._
 import com.gravity.goose.utils.FileHelper
-import  scala.collection.JavaConversions._
+import scala.collection.JavaConversions._
 
 object StopWords {
-  // the confusing pattern below is basically just match any non-word character excluding white-space.
-  private val PUNCTUATION: StringReplacement = StringReplacement.compile("[^\\p{Ll}\\p{Lu}\\p{Lt}\\p{Lo}\\p{Nd}\\p{Pc}\\s]", string.empty)
-
   val STOP_WORDS = FileHelper.loadResourceFile("stopwords-en.txt", StopWords.getClass).split("\n").toSet
 
   def getStopWordCount(content: String): WordStats = {
@@ -43,8 +40,10 @@ object StopWords {
     new WordStats(stopWordsInContent, candidateWords.length)
   }
 
-  private def extractCandidateWords(content: String): Array[String] = {
-    val strippedContent = PUNCTUATION.replaceAll(content)
-    string.SPACE_SPLITTER.split(strippedContent)
+  private def extractCandidateWords(content: String): Array[String] = string.SPACE_SPLITTER.split(stripPunctuation(content))
+
+  private def stripPunctuation(content: String) = {
+    val nonWordCharactersExcludingWhiteSpace = StringReplacement.compile("[^\\p{Ll}\\p{Lu}\\p{Lt}\\p{Lo}\\p{Nd}\\p{Pc}\\s]", string.empty)
+    nonWordCharactersExcludingWhiteSpace.replaceAll(content)
   }
 }
