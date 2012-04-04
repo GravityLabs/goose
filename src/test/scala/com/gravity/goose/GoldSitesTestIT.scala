5e2cbd978eb834da1f9e6dc7214e0b618fdf3691
Marco Singer <markaum@gmail.com>
2012-04-03 23:00:12 -0300

    added more two feeds in GoldSitesTest: folha and lancenet

--- src/test/scala/com/gravity/goose/GoldSitesTestIT.scala
+++ src/test/scala/com/gravity/goose/GoldSitesTestIT.scala
@@ -31,5 +31,38 @@ class GoldSitesTestIT {
     val article = TestUtils.getArticle(url)
     TestUtils.runArticleAssertions(article = article, expectedTitle = title, expectedImage = image)
   }
+
+  @Test
+  def folha() {
+    implicit val config = TestUtils.DEFAULT_CONFIG
+
+    val url     = "http://www1.folha.uol.com.br/esporte/1070420-leao-critica-regulamento-do-paulista-e-poe-culpa-na-tv.shtml"
+    val content = "Após retomar a liderança do Campeonato Paulista, com a vitória do São Paulo de virada por 4 a 2 sobre o Ituano"
+    val image   = "http://f.i.uol.com.br/folha/esporte/images/12084302.jpeg"
+    val title   = "Leão critica regulamento do Paulista e põe culpa na TV"
+    val article = TestUtils.getArticle(url)
+
+    TestUtils.runArticleAssertions(article = article,
+      expectedStart = content,
+      expectedTitle = title,
+      expectedImage = image)
+  }
+
+
+  @Test
+  def lancenet() {
+    implicit val config = TestUtils.DEFAULT_CONFIG
+
+    val url     = "http://www.lancenet.com.br/sao-paulo/Leao-Arena-Barueri-casa-Tricolor_0_675532605.html"
+    val content = "No próximo sábado, o São Paulo jogará, como mandante, na Arena Barueri diante do Mogi Mirim"
+    val image   = "http://www.lancenet.com.br/futebol-general/Paulo-Catanduvense-Campeonato-Paulista-Fernandinho_LANIMA20120329_0148_25.jpg"
+    val title   = "Para Leão, Arena Barueri não é casa do Tricolor"
+    val article = TestUtils.getArticle(url)
+
+    TestUtils.runArticleAssertions(article = article,
+      expectedStart = content,
+      expectedTitle = title,
+      expectedImage = image)
+  }
 }
 
