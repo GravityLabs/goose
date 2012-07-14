b4a16ba1893def2164d7bed775398f66be1b6930
Marco Singer <markaum@gmail.com>
2012-07-14 11:32:57 -0300

    removing support for ESPN website

--- src/test/scala/com/gravity/goose/GoldSitesTestIT.scala
+++ src/test/scala/com/gravity/goose/GoldSitesTestIT.scala
@@ -16,21 +16,9 @@ import com.gravity.goose.extractors.{VoicesContentExtractor, AdditionalDataExtra
   * Updated by Marco Singer
   * User: marcosinger
   * Date: 4/3/12
-  * This list is now just of soccer websites
+  * This list now just soccer websites
   */
 class GoldSitesTestIT {
-
-  @Test
-  def espn_brazil() {
-    implicit val config = TestUtils.DEFAULT_CONFIG
-
-    val url = "http://espn.estadao.com.br/saopaulo/noticia/248648_UM+ANO+APOS+APRESENTACAO+PARA+45+MIL+LUIS+FABIANO+ACUMULA+TRES+LESOES+E+14+GOLS"
-    val content = "Há exatamente um ano, cerca de 45 mil pessoas faziam festa no Morumbi"
-    val image = "http://contenti1.espn.com.br/foto/pequena/1d5b0d8f-1d44-3ade-a582-5c9d8700773c.gif"
-    val title = "Um ano após apresentação para 45 mil, Luis Fabiano acumula três lesões e 14 gols"
-    val article = TestUtils.getArticle(url)
-    TestUtils.runArticleAssertions(article = article, expectedTitle = title, expectedImage = image)
-  }
 
   @Test
   def folha() {
