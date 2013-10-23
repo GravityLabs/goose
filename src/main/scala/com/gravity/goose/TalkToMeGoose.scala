74f1e4d137f123255f44b35ba0aa5d69c68d24b1
Sravan Bhamidipati <bsravanin@gmail.com>
2013-10-22 22:31:31 -0400

    Changing the CLI interface to process a file containing a list of URLs, instead of a single URL.

--- src/main/scala/com/gravity/goose/TalkToMeGoose.scala
+++ src/main/scala/com/gravity/goose/TalkToMeGoose.scala
@@ -1,42 +1,34 @@
 package com.gravity.goose
 
-/**
- * Created by Jim Plush
- * User: jim
- * Date: 5/13/11
- */
+import java.io._
+import scala.io.Source
+
 object TalkToMeGoose {
-  /**
-  * you can use this method if you want to run goose from the command line to extract html from a bashscript
-  * or to just test it's functionality
-  * you can run it like so
-  * cd into the goose root
-  * mvn compile
-  * MAVEN_OPTS="-Xms256m -Xmx2000m"; mvn exec:java -Dexec.mainClass=com.gravity.goose.TalkToMeGoose -Dexec.args="http://techcrunch.com/2011/05/13/native-apps-or-web-apps-particle-code-wants-you-to-do-both/" -e -q > ~/Desktop/gooseresult.txt
-  *
-  * Some top gun love:
-  * Officer: [in the midst of the MIG battle] Both Catapults are broken, sir.
-  * Stinger: How long will it take?
-  * Officer: It'll take ten minutes.
-  * Stinger: Bullshit ten minutes! This thing will be over in two minutes! Get on it!
-  *
-  * @param args
-  */
-  def main(args: Array[String]) {
-    try {
-      val url: String = args(0)
-      val config: Configuration = new Configuration
-      config.enableImageFetching = false
-      val goose = new Goose(config)
-      val article = goose.extractContent(url)
-      println(article.cleanedArticleText)
-    }
-    catch {
-      case e: Exception => {
-        System.out.println("Make sure you pass in a valid URL: " + e.toString)
-      }
-    }
-  }
+	/**
+	 * Run Goose on a file containing a list of URLs.
+	 * mvn compile
+	 * mvn exec:java -Dexec.mainClass=com.gravity.goose.TalkToMeGoose -Dexec.args="path_to_url_file path_to_extract_to_dir"
+	 */
+
+	def main(args: Array[String]) {
+		val url_file: String = args(0)
+		val extracted: String = args(1)
+		val config: Configuration = new Configuration
+		config.enableImageFetching = false
+		val goose = new Goose(config)
+
+		for (url <- Source.fromFile(url_file).getLines()) {
+			try {
+				val article = goose.extractContent(url)
+				val filename = extracted + "/" + url.replace("http://", "").replace("https://", "").replace("/", "_") + "_" + System.currentTimeMillis
+				val writer = new PrintWriter(new File(filename))
+				writer.write(article.cleanedArticleText)
+				writer.close()
+			} catch {
+				case e: Exception => {
+					System.out.println(url + " is not a valid URL: " + e.toString)
+				}
+			}
+		}
+	}
 }
-
-
